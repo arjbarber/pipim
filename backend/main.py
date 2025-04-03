85e898580428839495de8a45151a81e8e2de99f2
arjbarber <arjbarber@gmail.com>
2025-04-03 12:03:10 -0400

    organized frontend and backend

---
+++ backend/main.py
@@ -0,0 +1,15 @@
+from flask import Flask, jsonify, request
+import subprocess
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return jsonify({"message": "Welcome to the Flask App!"})
+
+@app.route('/setup', methods=['GET'])
+def get_modules():
+    print(subprocess.run(['pip', 'list']))
+
+if __name__ == '__main__':
+    app.run(debug=True)
